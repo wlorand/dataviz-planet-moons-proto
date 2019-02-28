a1bdd85a73d407dbda497a76d60f93153c6c6544
Will <willlorand19@OpsRamps-MacBook-Pro.local>
2019-02-27 20:43:34 -0800

    imported solar system json

--- client/src/scripts/planet-moons-layout.js
+++ client/src/scripts/planet-moons-layout.js
@@ -4,14 +4,24 @@
  */
 
 // 0- require the Ogma Lib and assign to the window object for dev console access
-const Ogma = require('./vendor/ogma.min.js');
+const Ogma = require("./vendor/ogma.min.js");
 window.Ogma = Ogma;
-
 
 // 1- create a new Ogma instance
 const ogma = new Ogma({
-    container: 'graph-root',
-    renderer: 'webgl'
-  });
+  container: "graph-root",
+  renderer: "webgl"
+});
 
- 
+// 2- import some JSON data -- .then load into an ogma graph and apply force layout
+ogma.parse
+  //   .jsonFromUrl("./../mock-data/iphone_parts.json")
+  .jsonFromUrl("./../mock-data/solar-system.json")
+  .then(graph => ogma.setGraph(graph))
+  .then(() => ogma.layouts.forceLink({ locate: true }));
+
+
+// 3- add event listeners
+document.querySelector('#toggle-moons').addEventListener('click', (evt) => { 
+    alert(`you got clicked at x,y: ${evt.x}, ${evt.y}`);
+});
